76cdf736087ede18fb6927ba7db9095a5fd01a6e
vteramin <vteramin@gmail.com>
2024-02-08 19:34:47 +0300

    feat: add sidebar

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
-import { store } from '@redux/configure-store';
 import { MainPage } from './pages';
 
 import 'normalize.css';
@@ -14,12 +12,10 @@ const root = createRoot(domNode);
 
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <HashRouter>
-                <Routes>
-                    <Route path='/' element={<MainPage />} />
-                </Routes>
-            </HashRouter>
-        </Provider>
+        <HashRouter>
+            <Routes>
+                <Route path='/' element={<MainPage />} />
+            </Routes>
+        </HashRouter>
     </React.StrictMode>,
 );
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,31 +1,66 @@
 import React, { useState } from 'react';
-
-import reactLogo from '/react.svg';
-import viteLogo from '/vite.svg';
-import tsLogo from '/ts.svg';
 import './main-page.css';
+import 'antd/dist/antd.css';
+
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+import { MenuFoldOutlined, MenuUnfoldOutlined, CalendarOutlined, HeartFilled, TrophyFilled, IdcardOutlined } from '@ant-design/icons';
 
 export const MainPage: React.FC = () => {
-    const [count, setCount] = useState(0);
+    const [collapsed, setCollapsed] = useState(false);
 
     return (
-        <>
-            <div>
-                <a href='https://vitejs.dev' target='_blank'>
-                    <img src={viteLogo} className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://react.dev' target='_blank'>
-                    <img src={reactLogo} className='logo react' alt='React logo' />
-                </a>
-                <a href='https://www.typescriptlang.org/' target='_blank'>
-                    <img src={tsLogo} className='logo' alt='TS logo' />
-                </a>
-            </div>
-            <h1>Vite + React + TS</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-            </div>
-            <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-        </>
+        <Layout className="main-page">
+            <Sider
+                trigger={null}
+                collapsible
+                collapsed={collapsed}
+                width="208"
+                collapsedWidth="64"
+                theme="light"
+                className="main-page__sider sider"
+            >
+                <div
+                    className={`sider__logo ${collapsed ? "logo-collapsed" : "logo"}`}
+                    style={{ backgroundImage: `url(../../public/assets/logo/${collapsed ? "logoCollapsed" : "logo"}.svg)` }}
+                />
+                <Menu
+                    theme="light"
+                    mode="inline"
+                    defaultSelectedKeys={['4']}
+                    className="sider__menu"
+                    items={[CalendarOutlined, HeartFilled, TrophyFilled, IdcardOutlined].map((icon, index) => ({
+                        key: String(index + 1),
+                        icon: React.createElement(icon, {
+                            style: { color: "var(--primary-light-9)", fontSize: "14px" },
+                            className: "menu__icon"
+                        }),
+                        label: ["Календарь", "Тренировки", "Достижения", "Профиль"][index],
+                        style: collapsed ? {} : { paddingLeft: "16px" },
+                        className: "menu__item"
+                    })
+                    )}
+                />
+                <div className="sider__exit">
+                    <div className="exit__icon"></div>
+                    {!collapsed && <p className="exit__line">Выход</p>}
+                </div>
+            </Sider>
+            <Layout className="main-page__wrapper">
+                <Header className="site-layout-sub-header-background" style={{ padding: 0 }}>
+                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                        className: "trigger",
+                        style: { color: "var(--neutral-gray-7)" },
+                        onClick: () => setCollapsed(!collapsed)
+                    })}
+                </Header>
+                <Content style={{ margin: '24px 16px 0' }} className="content">
+                    <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                        content
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+            </Layout>
+        </Layout>
     );
 };
