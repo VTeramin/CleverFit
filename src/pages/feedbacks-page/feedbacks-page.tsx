8f45675c0d47be372d0ec8af6d8de0e3959c5b3b
vteramin <vteramin@gmail.com>
2024-02-29 13:55:11 +0300

    feat: add no feedback case

--- src/pages/feedbacks-page/feedbacks-page.tsx
+++ src/pages/feedbacks-page/feedbacks-page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 import styles from './feedbacks-page.module.css';
 import { Avatar, Button, Card, Comment, Layout, Rate } from 'antd';
@@ -7,43 +7,51 @@ import { store } from '@redux/configure-store';
 
 export const FeedbacksPage: React.FC = () => {
     const cardsData = store.getState().feedback;
+    const [isNoFeedback, setIsNoFeedback] = useState(cardsData.length === 0);
     function convertDate(date: string) {
         return date.substring(0, 10).split("-").reverse().join(".");
     }
 
-    const cards = cardsData.map((cardData, ind) => (
-        <Card key={ind} className={styles["card"]}>
-            <Comment
-                avatar={<div className={styles["card__person"]}>
-                    <Avatar
-                        src={cardData.imageSrc || <UserOutlined />}
-                        alt={cardData.fullName}
-                        size={42}
-                        className={styles["person__img"]}
-                    />
-                    <p className={styles["person__name"]}>{cardData.fullName}</p>
-                </div>}
-                author={<Rate
-                    disabled
-                    value={cardData.rating}
-                    character={<StarTwoTone twoToneColor="var(--character-light-warning)" className={styles["rate__star"]} />}
-                    className={styles["card__rate"]}
-                />}
-                datetime={<span className={styles["card__time"]}>{convertDate(cardData.createdAt)}</span>}
-                content={<p>{cardData.message}</p>}
-                className={styles["card__comment"]}
-            />
-        </Card>
-    ));
+    const firstFeedback = (
+        <div className={styles["firstFeedback"]}>
+            <h2 className={styles["firstFeedback__title"]}>Оставьте свой отзыв первым</h2>
+            <p className={styles["firstFeedback__subtitle"]}>Вы можете быть первым, кто оставит отзыв об этом фитнесс приложении.<br />Поделитесь своим мнением и опытом с другими пользователями,<br />и помогите им сделать правильный выбор.</p>
+        </div>
+    );
+
+    const cards = <section className={styles["page__cards"]}>
+        {cardsData.map((cardData, ind) => (
+            <Card key={ind} className={styles["card"]}>
+                <Comment
+                    avatar={<div className={styles["card__person"]}>
+                        <Avatar
+                            src={cardData.imageSrc || <UserOutlined />}
+                            alt={cardData.fullName}
+                            size={42}
+                            className={styles["person__img"]}
+                        />
+                        <p className={styles["person__name"]}>{cardData.fullName}</p>
+                    </div>}
+                    author={<Rate
+                        disabled
+                        value={cardData.rating}
+                        character={<StarTwoTone twoToneColor="var(--character-light-warning)" className={styles["rate__star"]} />}
+                        className={styles["card__rate"]}
+                    />}
+                    datetime={<span className={styles["card__time"]}>{convertDate(cardData.createdAt)}</span>}
+                    content={<p>{cardData.message}</p>}
+                    className={styles["card__comment"]}
+                />
+            </Card>
+        )).slice(0, 4)}
+    </section>;
 
     return (
-        <Layout className={styles["page"]}>
-            <section className={styles["page__cards"]}>
-                {cards.slice(0, 4)}
-            </section>
+        <Layout className={`${styles["page"]} ${styles[isNoFeedback ? "no-feedback" : ""]}`}>
+            {isNoFeedback ? firstFeedback : cards}
             <div className={styles["page__buttons"]}>
-                <Button className={styles["conf-button"]}>Написать отзыв</Button>
-                <Button type="text" className={styles["text-button"]}>Развернуть все отзывы</Button>
+                <Button className={`${styles["conf-button"]} ${styles[isNoFeedback ? "no-feedback" : ""]}`}>Написать отзыв</Button>
+                {!isNoFeedback && <Button type="text" className={styles["text-button"]}>Развернуть все отзывы</Button>}
             </div>
         </Layout>
     );
