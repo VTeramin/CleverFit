baeb6edfd8f604e0db67fa2d3822d29aecde61df
vteramin <vteramin@gmail.com>
2024-03-16 16:07:29 +0300

    refactor: delete unnecessary folders

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Layout } from 'antd';
-import { MainContent } from './components/main-content/main-content';
-import { MainFooter } from './components/main-footer/main-footer';
+import { MainContent } from './main-content/main-content';
+import { MainFooter } from './main-footer/main-footer';
 
 export const MainPage: React.FC = () => {
     return (
