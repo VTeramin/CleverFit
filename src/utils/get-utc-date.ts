4a53348edea761aaa3df1f727f87b3d4076e79c7
vteramin <vteramin@gmail.com>
2024-04-12 03:29:15 +0300

    fix: bild

--- src/utils/get-utc-date.ts
+++ src/utils/get-utc-date.ts
@@ -1,3 +1,10 @@
 export function getUTCDate(date: Date) {
-    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
+    return new Date(Date.UTC(
+        date.getFullYear(),
+        date.getMonth(),
+        date.getDate(),
+        date.getHours(),
+        date.getMinutes(),
+        date.getSeconds()
+    ));
 }
